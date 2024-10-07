0a3cf52492bc37c0f816bddafc947570940d05ec
poketopa <96972872+Poketopa@users.noreply.github.com>
2024-10-07 23:51:19 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.50 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,49 +1,34 @@
+from datetime import datetime, timedelta
+
 def solution(today, terms, privacies):
+    # 답
     answer = []
+    # 약관과 달 저장
+    terms2 = []
+    privacies2 = []
     
-    
-    # 오늘의 날짜를 배열로 저장 [2022, 05, 19]
-    todayDate = today.split(".")
-    # 정수형으로 변경
-    todayDate = [int(i) for i in todayDate]
-    
-    # 약관의 유효기간을 딕셔너리로 저장
-    contract = {}
-    for i in terms:
-        x, y = i.split()
-        contract[x] = y
-        
-    
-    for i in range(0,len(privacies)):
-        # 날짜와 약관을 나눈다 
-        temp = privacies[i].split()
-        # 날짜를 배렬로 저장
-        tempDate = temp[0].split(".")
-        tempDate = [int(i) for i in tempDate]
-        # 약관이 무엇인지 저장
-        tempContract = temp[1]
-        # 달에 약관의 달을 더한다
-        tempDate[1] += int(contract[tempContract])
-        # 달이 12가 넘어갈 경우 년도를 변경
-        # 만약 달이 12의 배수면 1뺀 뒤 연산하고 다시 더함
-        if tempDate[1] % 12 == 0:
-            tempDate[0] += (tempDate[1] // 12)  -1
-            tempDate[1] = 12
-        else:
-            tempDate[0] += tempDate[1] //12
-            tempDate[1] = tempDate[1] % 12
-        
-        if tempDate[0]<todayDate[0]:
-            answer.append(i+1)
-            continue
-        elif tempDate[0] == todayDate[0]:
-            if tempDate[1]<todayDate[1]:
-                answer.append(i+1)
-                continue
-            elif tempDate[1] == todayDate[1]:
-                if tempDate[2]<=todayDate[2]:
-                    answer.append(i+1)
-                else:
-                    continue
+    # [["A","6"], ["B", "12"], ["C", "3"]]
+    for i in terms :
+        terms2.append(i.split())
+    for i in privacies : 
+        privacies2.append(i.split())
+
+        # 오늘 날짜 배열 저장
+        today2 = today.split(".")
+        # 1일 기준 저장
+        todayDate = int(today2[0]) * 12 * 28 + int(today2[1]) * 28 + int(today2[2])
+
+    count = 1
+
+    for i in privacies2 : 
+        # 계약 날짜 나누기
+        expire = i[0].split(".")
+        for j in terms2 :
+            if i[1] == j[0] :
+                expireDate = int(expire[0]) * 12*28 + int(expire[1]) * 28 + int(j[1]) * 28 + int(expire[2])
+                break
+        if todayDate >= expireDate :
+            answer.append(count)
+        count += 1
 
     return answer
