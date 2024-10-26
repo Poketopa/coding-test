8f87cf6eda1fa4e7f63429608fed51508c164103
poketopa <96972872+Poketopa@users.noreply.github.com>
2024-10-26 23:53:16 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.55 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int len = array.length;
+        return array[(len-1)/2];
+    }
+}
