271b6890c955e02eeed7d9e1b59f8249758b9763
poketopa <96972872+Poketopa@users.noreply.github.com>
2024-11-02 16:50:38 +0900

    [level 0] Title: 숫자 찾기, Time: 0.18 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int k) {
+        String[] str = String.valueOf(num).split("");
+        for(int i=0;i<str.length;i++){
+            if(str[i].equals(String.valueOf(k))){
+                return i+1;
+            }
+        }
+        return -1;
+        
+    }
+}
