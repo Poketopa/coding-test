28fb2303240e0a4190b0be46d3b073ee10a46326
poketopa <96972872+Poketopa@users.noreply.github.com>
2024-11-03 09:48:49 +0900

    [level 0] Title: 외계행성의 나이, Time: 11.00 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int age) {
+        String str = String.valueOf(age);
+        String answer = "";
+        for(int i=0;i<str.length();i++){
+            answer+=(char)(str.charAt(i)+49);
+        }
+        
+        return answer;
+    }
+}
