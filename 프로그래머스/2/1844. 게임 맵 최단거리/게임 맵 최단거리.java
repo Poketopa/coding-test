075cc4b5b526ad36a9cd788809027963d7453c5a
poketopa <96972872+Poketopa@users.noreply.github.com>
2024-11-20 15:07:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.99 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,67 +1,38 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[][] maps) {
-        int xLength = maps.length;
-        int yLength = maps[0].length;
+        int[][] move = new int[maps.length][maps[0].length];
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0});
+        move[0][0]=1;
+
+        int distance = 0;
         int nowX = 0;
         int nowY = 0;
-        
-        int move[][] = new int[xLength][yLength];
-        boolean visited[][] = new boolean[xLength][yLength];
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-        move[0][0] = 1;
-        visited[0][0] = true;
-        
-        
-        while (!queue.isEmpty()) {
-            int[] xy = queue.poll();
-            int x = xy[0];
-            int y = xy[1];
+        int[] x = {1,-1,0,0};
+        int[] y = {0, 0,1,-1};
 
-            if (x == xLength - 1 && y == yLength - 1) {
-                return move[x][y];
-            }
+        while(queue.size()!=0){
+            int[] now = queue.poll();
+            maps[now[0]][now[1]]=0;
 
-            if (x + 1 < xLength && maps[x + 1][y] == 1) {
-                if (!visited[x + 1][y]) {
-                    queue.offer(new int[]{x + 1, y});
-                    move[x + 1][y] = move[x][y] + 1;
-                    visited[x+1][y] = true;
+            for(int i=0;i<4;i++){
+                int dx = now[0]+x[i];
+                int dy = now[1]+y[i];
+                if(dx>=0&&dx<maps.length&&dy>=0&&dy<maps[0].length&&maps[dx][dy]==1&&visited[dx][dy] == false){
+                    queue.offer(new int[]{dx,dy});
+                    move[dx][dy] = move[now[0]][now[1]]+1;
+                    visited[dx][dy] = true;
+                    if(dx==maps.length-1&&dy==maps[0].length-1){
+                        return move[dx][dy];
+                    }
                 }
-
             }
-            if (x > 0 && maps[x - 1][y] == 1) {
-                if (!visited[x - 1][y]) {
-                    queue.offer(new int[]{x - 1, y});
-                    move[x - 1][y] = move[x][y] + 1;
-                    visited[x-1][y] = true;
-                }
 
-            }
-            if (y + 1 < yLength && maps[x][y + 1] == 1) {
-                if (!visited[x][y + 1]) {
-                    queue.offer(new int[]{x, y + 1});
-                    move[x][y + 1] = move[x][y] + 1;
-                    visited[x][y+1] = true;
-                }
-
-            }
-            
-            if (y > 0 && maps[x][y - 1] == 1) {
-                if (!visited[x][y - 1]) {
-                    queue.offer(new int[]{x, y - 1});
-                    move[x][y - 1] = move[x][y] + 1;
-                    visited[x][y-1] = true;
-
-                }
-
-            }
         }
         return -1;
     }
-
 }
