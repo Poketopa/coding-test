9633bc4a50ec3a864496b8ee77e218dea9d5c465
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-01-25 21:23:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 18.56 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,56 +1,38 @@
-import java.util.*;
-
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int videoLength = timeTransfer(video_len);
-        int now = timeTransfer(pos);
-        int openingStart = timeTransfer(op_start);
-        int openingEnd = timeTransfer(op_end);
+        // pos : 시작지점
+        int video_length = changeTime(video_len);
+        int now = changeTime(pos);
+        int openingStart = changeTime(op_start);
+        int openingEnd = changeTime(op_end);
 
         for(int i=0;i<commands.length;i++){
-            if(openingStart<=now && now<=openingEnd){
+            if(now < 0) now = 0;
+            if(now > video_length) now = video_length;
+            if(openingStart <= now && now <= openingEnd){
                 now = openingEnd;
             }
-            if(commands[i].equals("prev")){
-                now -= 10;
-                if(now<0) now = 0;
-            }
-            else if(commands[i].equals("next")){
-                now+=10;
-                if(now>videoLength) now = videoLength;
-            }
-            if(openingStart<=now && now<=openingEnd){
-                now = openingEnd;
-            }
-        }
-        int hour = now / 60;
-        int minute = now % 60;
-        String answer = "";
+            if(commands[i].equals("next")) now += 10;
+            else if(commands[i].equals("prev")) now -= 10;
 
-        if(hour / 10 == 0){
-            answer+="0";
-            answer += String.valueOf(hour);
         }
-        else{
-            answer += String.valueOf(hour);
-        }
-        answer += ":";
-        if(minute / 10 == 0){
-            answer+="0";
-            answer += String.valueOf(minute);
-        }
-        else{
-            answer += String.valueOf(minute);
+        if(now < 0) now = 0;
+        if(now > video_length) now = video_length;
+        if(openingStart <= now && now <= openingEnd){
+            now = openingEnd;
         }
 
-        return answer;
+        String hour = String.valueOf(now / 60);
+        String minute = String.valueOf(now % 60);
+        if(hour.length() == 1) hour = "0" + hour;
+        if(minute.length() == 1) minute = "0" + minute;
+        return hour + ":" + minute;
     }
 
-    private int timeTransfer(String input){
-        String[] temp = input.split(":");
-        int time = 0;
-        time += Integer.valueOf(temp[0]) * 60;
-        time += Integer.valueOf(temp[1]);
-        return time;
+    private int changeTime(String time){
+        String[] temp = time.split(":");
+        int hour = Integer.valueOf(temp[0]);
+        int minute = Integer.valueOf(temp[1]);
+        return (hour * 60) + minute;
     }
 }
