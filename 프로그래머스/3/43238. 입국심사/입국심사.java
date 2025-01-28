151e388d42b0ca0343fd4946095709dfa0b0a2a2
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-01-28 11:30:55 +0900

    [level 3] Title: 입국심사, Time: 57.00 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,32 +1,31 @@
 class Solution {
     public long solution(int n, int[] times) {
-        // max 찾기
-        long min = 0;
-        long max = -1;
+        // min : 최소시간 (초기값 0)
+        // max : 최대시간 (초기값 가장 긴 시간 * n)
+        // mid = 이분 탐색을 위한 가운데 점
+        long min = -1;
+        long max = 0;
         for(int i : times) if(max<i) max = i;
         max *= n;
         long answer = 0;
 
-        // 1. 최저시간, 최대시간을 설정한다.
-        // 2. 각 시간을 나눠서 더한 뒤 n보다 크면 최대시간을 줄인다
-        //      n보다 작으면 최저시간을 늘린다
-        if(times.length == 1)  return (long)n * times[0];
-        while(min<=max){
-            long mid = (max + min) / 2;
-            long sum = 0;
-            for(int i : times) {
-                sum += (mid / i);
-                if(sum >= n) break;
-            }
+        // 1. mid시간을 기준으로 times로 나누어서 n과 비교한다
+        // n >= sum / 모든 사람을 검사하지 못했다 -> 시간 부족하므로 mid 상승
+        // n < sum / 모든 사람을 검사하고 시간이 남음 -> mid 줄인다
+        // 2. 시간이 부족한 경우 min = mid + 1로 설정해여서 크기가 작을 때 min > max인 상황을 만든다
 
-            // 시간이 부족한 경우
-            if(n>sum){
-                min = mid + 1;
-            }
-            // 시간이 충분한 경우
-            else if(n<=sum){
-                answer = mid;
+        while(min <= max){
+            long mid = (min + max) / 2;
+            long sum = 0;
+            for(int i : times) sum += mid / i;
+
+            if(n <= sum){
                 max = mid - 1;
+                    answer = mid;
+
+            }
+            else{
+                min = mid + 1;
             }
         }
         return answer;
