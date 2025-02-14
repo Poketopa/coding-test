dbee2792dab1ea4369bfda75992a92bdb8d2d90e
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-14 23:29:41 +0900

    [Silver II] Title: 나무 자르기, Time: 492 ms, Memory: 122940 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,41 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int trees = Integer.parseInt(st.nextToken());
+        int need = Integer.parseInt(st.nextToken());
+        int[] length = new int[trees];
+        int max = -1;
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=0;i<length.length;i++){
+            length[i] = Integer.valueOf(st2.nextToken());
+            max = Math.max(max, length[i]);
+        }
+        int start = 0;
+        int end = max;
+        int answer = 0;
+        while(start<=end){
+            int mid = (start + end) / 2;
+            long sum = 0;
+            for(int i=0;i<length.length;i++){
+                // 나무가 작으면 안잘림
+                if(mid >= length[i]) continue;
+                sum += length[i] - mid;
+            }
+            if(sum >= need){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        System.out.println(start-1);
+    }
+}
