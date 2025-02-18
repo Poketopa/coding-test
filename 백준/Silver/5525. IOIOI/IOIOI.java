39ccb9da89723bb966f28fe6a3002c9a9051c2cf
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-18 10:27:29 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 24420 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -11,14 +11,28 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int length = Integer.parseInt(br.readLine());
         StringBuilder str = new StringBuilder(br.readLine());
-        String target = "";
-        for(int i=0;i<n;i++) target += "IO";
-        target += "I";
+        StringBuilder target = new StringBuilder();
+        for(int i=0;i<n;i++) target.append("IO");
+        target.append("I");
+        String answer = target.toString();
 
-        int count = 0;
-        for(int i=0;i<=str.length()-target.length();i++){
-            if(str.substring(i, i+target.length()).equals(target)) count++;
+        int index = 0;
+        int result = 0;
+        while(index<=str.length()-answer.length())
+        {
+            int count = 0;
+            if(str.charAt(index) == 'I'){
+                index++;
+                while(index < (str.length()-1) &&str.charAt(index) == 'O' && str.charAt(index+1) == 'I'){
+                    count++;
+                    index+=2;
+                }
+                if(count >= n){
+                    result += count-n + 1;
+                }
+            }
+            else index++;
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
