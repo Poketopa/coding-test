0779fbd0f68f4a0e1175b2c5cef99587f12e6169
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-19 19:29:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,15 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        long input = Integer.parseInt(br.readLine());
+        System.out.println(input*input*input);
+        System.out.println("3");
+
+    }
+}
