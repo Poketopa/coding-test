8dcfdb8d99b17c3aa8306e1574f8ea40b5ee0432
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-21 21:14:32 +0900

    [Bronze IV] Title: 특별한 가지, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int num3 = Integer.parseInt(br.readLine());
+
+        System.out.println(num2/num1*num3);
+    }
+}
