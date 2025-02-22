1a5cda44f9957a841e67955aef3a06d22c9fae0b
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-22 13:11:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.84 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,14 +1,19 @@
-
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<participant.length;i++){
+            map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
+        }
         for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
+            map.put(completion[i], map.get(completion[i])-1);
+        }
+        for(int i=0;i<participant.length;i++){
+            if(!map.containsKey(participant[i]) || map.get(participant[i]) >0){
                 return participant[i];
             }
         }
-        return participant[participant.length-1];
+        return "";
     }
 }
