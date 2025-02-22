5af57b6493fdfa0a543956f5f9ab1c314c692b0d
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-22 11:39:41 +0900

    [level 2] Title: 피로도, Time: 4.29 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,25 @@
-import java.util.*;
 
+import java.util.*;
 class Solution {
     static int max = -1;
-
     public int solution(int k, int[][] dungeons) {
         int[] visited = new int[dungeons.length];
-        dfs(k, dungeons, 0, visited);
+        int count = 1;
+        for(int i=0;i<dungeons.length;i++){
+            dfs(visited, count, k, dungeons, i);
+        }
         return max;
     }
 
-    private static void dfs(int hp, int[][] dungeons, int count, int[] visited){
+    private void dfs(int[] visited, int count, int k, int[][] dungeons, int now){
+        if(visited[now] == 1) return;
+        if(k<dungeons[now][0]) return;
         max = Math.max(max, count);
+        visited[now] = 1;
 
-        for(int i=0;i<visited.length;i++){
-            // 이미 왔을 때
-            if(visited[i]==1) {
-                continue;
-            }
-            // 체력 없어서 못 들어갈 때
-            if(hp<dungeons[i][0]){
-                continue;
-            }
-            visited[i] = 1;
-            dfs(hp-dungeons[i][1], dungeons, count+1, visited);
-            visited[i] = 0;
+        for(int i=0;i<dungeons.length;i++){
+            dfs(visited, count + 1, k - dungeons[now][1], dungeons, i);
         }
+        visited[now] = 0;
     }
 }
