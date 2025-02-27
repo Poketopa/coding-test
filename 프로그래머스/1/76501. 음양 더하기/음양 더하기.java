c428f912e38d5537870d09f239622d1f686af488
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-02-27 23:52:58 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for (int i=0;i<absolutes.length;i++){
-            if (signs[i] == true){
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i] == true){
                 answer+=absolutes[i];
             }
             else{
