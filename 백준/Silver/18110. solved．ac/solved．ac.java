6d3060657bad662b4614207f0574d6b25c6661fc
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-03-26 21:17:34 +0900

    [Silver IV] Title: solved.ac, Time: 316 ms, Memory: 38128 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -3,27 +3,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //StringTokenizer st = new StringTokenizer(br.readLine());
-        int input = Integer.valueOf(br.readLine());
-        int[] number = new int[input];
-        for(int i=0;i<input;i++){
-            number[i] = Integer.valueOf(br.readLine());
+        int input = Integer.parseInt(br.readLine());
+        int[] num = new int[input];
+        for(int i=0;i<input;i++) num[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(num);
+        double temp = input * 0.15;
+        int temp2 = 0;
+        if(temp % 1 >= 0.5) temp2 = (int)temp + 1;
+        else temp2 = (int)temp;
+        double avg = 0;
+        for(int i=temp2;i<num.length-temp2;i++){
+            avg += num[i];
         }
-        Arrays.sort(number);
-        double no = input * 0.15;
-        int noInt = 0;
-        if(no % 1 <0.5) noInt = (int)no;
-        else noInt = (int)(no+1);
-
-        int sum = 0;
-        for(int i=noInt;i<number.length-noInt;i++){
-            sum+=number[i];
+        avg /= num.length - 2 * temp2;
+        if(avg % 1 >= 0.5) System.out.println((int)avg + 1);
+        else System.out.println((int)avg);
         }
-        double result = (double)sum / (number.length - (2 * noInt));
-        if(result%1<0.5) System.out.println((int)result);
-        else System.out.println((int)(result+1));
-    }
 }
