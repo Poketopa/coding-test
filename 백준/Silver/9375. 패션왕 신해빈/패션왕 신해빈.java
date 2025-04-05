23e5b8ada8c6d19c15a63b4b6750632b5fd563de
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-04-05 23:01:42 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -3,26 +3,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int count0 = 0;
-    static int count1 = 0;
     public static void main(String[] args) throws IOException {
+        // 입력 처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //StringTokenizer st = new StringTokenizer(br.readLine());
-        int input = Integer.valueOf(br.readLine());
+        int input = Integer.parseInt(br.readLine());
+
         for(int i=0;i<input;i++){
-            int temp = Integer.valueOf(br.readLine());
-            HashMap<String, Integer> map = new HashMap<>();
+            int temp = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
             for(int j=0;j<temp;j++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String name = st.nextToken();
                 String wear = st.nextToken();
-                map.put(wear, map.getOrDefault(wear, 1)+1);
+                map.put(wear, map.getOrDefault(wear, 0) + 1);
             }
-            Set<String> set = new HashSet<>();
-            set = map.keySet();
-            int sum = 1;
-            for(String k : set){
-                sum *= map.get(k);
+            Set<String> set = new HashSet<>(map.keySet());
+            List<Integer> list = new ArrayList<>();
+            for(String j : set){
+                list.add(map.get(j));
+            }
+            long sum = 1;
+            for(int j : list){
+                sum *= (j + 1);
             }
             System.out.println(sum-1);
         }
