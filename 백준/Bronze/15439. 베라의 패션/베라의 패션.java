ecf32e103c81374a8070edb2a19a97f982b1652d
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-04-15 21:01:00 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int input = Integer.parseInt(br.readLine());
+        System.out.println((input-1) * input);
+    }
+}
