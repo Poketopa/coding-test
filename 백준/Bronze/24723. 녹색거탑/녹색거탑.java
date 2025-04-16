0980bba2772698bed31d16ccd3cb50819054228b
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-04-16 13:01:34 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int input = Integer.parseInt(br.readLine());
+        System.out.print((int)Math.pow(2, input));
+
+    }
+}
