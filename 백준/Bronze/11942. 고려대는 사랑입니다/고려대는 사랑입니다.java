fb66b15aaab6e1a5aa0bf1373961166ea219d3dd
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-06-30 08:24:27 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean flag = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println("고려대학교");
+    }
+}
