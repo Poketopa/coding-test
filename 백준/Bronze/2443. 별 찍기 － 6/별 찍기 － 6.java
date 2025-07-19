b367b2661a6e524a8704fbbc303cf5d7ff0db8dc
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-07-19 22:50:35 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 268 ms, Memory: 18228 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long pleasure = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int input = Integer.parseInt(br.readLine());
+        for(int i=0;i<input;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(input - i) * 2 - 1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
