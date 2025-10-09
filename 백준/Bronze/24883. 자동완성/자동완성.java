48e1592c44aa9f30aabea0ab1f1027a059648b12
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-10-09 11:48:43 +0900

    [Bronze V] Title: 자동완성, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//StringTokenizer st = new StringTokenizer(br.readLine());
+
+		String input = br.readLine();
+		
+		if(input.equals("N") || input.equals("n"))
+			System.out.print("Naver D2");
+		else 
+			System.out.print("Naver Whale");
+	}
+}
+
