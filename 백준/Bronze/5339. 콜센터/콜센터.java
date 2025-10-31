fe82b471897b15ec3c2dc51f6392f33151a8887c
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-10-31 22:09:13 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println("     /~\\\n"
+                + "    ( oo|\n"
+                + "    _\\=/_\n"
+                + "   /  _  \\\n"
+                + "  //|/.\\|\\\\\n"
+                + " ||  \\ /  ||\n"
+                + "============\n"
+                + "|          |\n"
+                + "|          |\n"
+                + "|          |");
+    }
+}
+
+
+
+
+
+
