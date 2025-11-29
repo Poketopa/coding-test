dc4cc354d6048adb933c853e341e901d9bdfa9e7
poketopa <96972872+Poketopa@users.noreply.github.com>
2025-11-29 22:26:03 +0900

    [Bronze V] Title: Hello Judge, Time: 120 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int input = Integer.parseInt(br.readLine());
+        for(int i=0;i<input;i++){
+            System.out.println("Hello World, Judge " + (i + 1) + "!");
+        }
+    }
+}
