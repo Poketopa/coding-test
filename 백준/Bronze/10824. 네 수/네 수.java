d9c47d5d0bf033409a44c67f388849a50ff2ea64
poketopa <96972872+Poketopa@users.noreply.github.com>
2026-01-05 17:11:33 +0900

    [Bronze III] Title: 네 수, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+
+        String ab = a + b;
+        String cd = c + d;
+
+        long i = Long.parseLong(ab);
+        long j = Long.parseLong(cd);
+
+        System.out.print(i + j);
+    }
+}
