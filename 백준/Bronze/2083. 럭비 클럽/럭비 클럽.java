681c996bdbbf72c62405aa644de17954e28a923a
poketopa <96972872+Poketopa@users.noreply.github.com>
2026-01-30 23:52:46 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            if(name.equals("#") && age == 0 && weight == 0) return;
+            if(age > 17 || weight >= 80) System.out.println(name + " Senior");
+            else System.out.println(name + " Junior");
+        }
+    }
+}
