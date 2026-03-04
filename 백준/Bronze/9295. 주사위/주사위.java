b0d4603826cb18f8bd22e35e4009772dd0639f05
Poketopa <96972872+Poketopa@users.noreply.github.com>
2026-03-04 21:36:47 +0900

    [Bronze IV] Title: 주사위, Time: 168 ms, Memory: 16612 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int input = Integer.parseInt(br.readLine());
+        for(int i=0;i<input;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            System.out.println("Case " + (i + 1) + ": " + sum);
+        }
+    }
+}
