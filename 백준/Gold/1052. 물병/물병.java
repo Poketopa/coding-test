543864c587ecd050c7c63069956076c9bdd1cd14
poketopa <96972872+poketopa@users.noreply.github.com>
2026-03-30 15:44:57 +0900

    [Gold V] Title: 물병, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.java
+++ 백준/Gold/1052. 물병/물병.java
@@ -1,42 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //StringTokenizer st = new StringTokenizer(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long sum = Long.parseLong(st.nextToken());
-        long bottle = Long.parseLong(st.nextToken());
+        int water = Integer.parseInt(st.nextToken());
+        int bottle = Integer.parseInt(st.nextToken());
 
         while(true){
-            long two = 1;
-
-            while(two <= sum){
-                two *= 2;
-            }
-            two /= 2;
-
-            sum -= two;
+            int maxWater = makeMaxWater(water);
+            water -= maxWater;
             bottle--;
-
             if(bottle == 0){
-                if(sum == 0){
+                if(water <= bottle){
                     System.out.print("0");
-                } else  System.out.print(two - sum);
-                return;
+                } else {
+                    System.out.print(maxWater - water);
+                }
+                break;
             }
         }
+    }
+
+    public static int makeMaxWater(int water){
+        int temp = 2;
+        while(true){
+            if(temp * 2 > water) break;
+            temp *= 2;
+        }
+
+        return temp;
     }
 }
