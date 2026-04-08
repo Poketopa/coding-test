bc3177da9c6b0c5d85fe0619480e848b4085f5c3
poketopa <96972872+poketopa@users.noreply.github.com>
2026-04-08 22:23:19 +0900

    [Silver V] Title: 정말 좋은 압축, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/5043. 정말 좋은 압축/정말 좋은 압축.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        if(a > (long)Math.pow(2, b + 1) - 1){
+            System.out.print("no");
+        }
+        else System.out.print("yes");
+    }
+}
